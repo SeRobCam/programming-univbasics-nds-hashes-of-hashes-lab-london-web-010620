05254d75b0f1f12d539ee202db42d16e6157440c
Sergio Robles <sroblesc@gmail.com>
2019-12-18 02:48:43 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -47,8 +47,8 @@ def naming_system
     :label => "Kingdom",
       :sub_category => {:label => "Phylum"},
       :sub_category => {:label => "Class", :label => "Order",:label => "Family"}
-      :sub_category => {:label: "Genus", :label => "Species"},
-      :sub_category => nil
+      :sub_category => {:label => "Genus", :label => "Species"},
+      :sub_category => {}
     
       }
     
